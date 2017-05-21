7d8a1716d085634afab666873461afaed52bff4a
yamanobori_old <yamanobori_old@yahoo.co.jp>
2017-05-21 11:38:13 +0900

    最初のバージョン

--- PosgresSQLSample/Program.cs
+++ PosgresSQLSample/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Npgsql;
 
 namespace PosgresSQLSample
 {
@@ -10,6 +8,35 @@ namespace PosgresSQLSample
     {
         static void Main(string[] args)
         {
+            var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // 雪が２０センチ以上の日
+                using (var cmd = new NpgsqlCommand("SELECT * FROM matsumoto_temp where snow >= 20 order by month", conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    var sb = new StringBuilder();
+                    while (reader.Read())
+                    {
+                        sb.Append(reader.GetDate(0)).Append("\t")
+                          .Append(reader.GetDouble(1)).Append("\t")
+                          .Append(reader.GetInt32(2)).Append("\t")
+                          .Append(reader.GetInt32(3)).Append("\t")
+                          .Append(reader.GetDouble(4)).Append("\t")
+                          .Append(reader.GetInt32(5)).Append("\t")
+                          .Append(reader.GetInt32(6)).Append("\t")
+                          .Append(reader.GetInt32(7)).Append("\t")
+                          .Append(reader.GetInt32(8)).Append("\t")
+                          .Append(reader.GetInt32(9)).Append("\t")
+                          .Append(reader.GetInt32(10)).Append("\t")
+                          .AppendLine();
+                    }
+                    Console.WriteLine(sb.ToString());
+                }
+            }
         }
     }
 }
