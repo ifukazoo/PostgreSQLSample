c602a13dd564ab1c344a1daafc8776d1e3c29769
yamanobori_old <yamanobori_old@yahoo.co.jp>
2017-07-29 21:53:41 +0900

    標準入力からsqlを読むようにした.

--- PostgreSQLSample/Program.cs
+++ PostgreSQLSample/Program.cs
@@ -1,70 +1,65 @@
 ﻿using Npgsql;
 using System;
 using System.Configuration;
-using System.Data.Linq;
-using System.Text;
+using System.Data.Common;
 
 namespace PostgreSQLSample
 {
     class Program
-  {
-    class TempRecord
     {
-      // 列   |      型      |  修飾語
-      // --------+--------------+----------
-      // month  | date         | not null
-      // max    | numeric(3,1) |
-      // max_q  | integer      |
-      // max_v  | integer      |
-      // min    | numeric(3,1) |
-      // min_q  | integer      |
-      // min_v  | integer      |
-      // snow   | integer      |
-      // snow_e | integer      |
-      // snow_q | integer      |
-      // snow_v | integer      |
-      internal DateTime month { get; set; }
-      internal Double max { get; set; }
-      internal int max_q { get; set; }
-      internal int max_v { get; set; }
-      internal Double min { get; set; }
-      internal int min_q { get; set; }
-      internal int min_v { get; set; }
-      internal int snow { get; set; }
-      internal int snow_e { get; set; }
-      internal int snow_q { get; set; }
-      internal int snow_v { get; set; }
-    }
 
-    static void Main(string[] args)
-    {
-      var connString = ConfigurationManager.ConnectionStrings["db"]?.ConnectionString;
-      using (var conn = new NpgsqlConnection(connString))
-      {
-        conn.Open();
-
-        // 雪が２０センチ以上の日
-        using (var context = new DataContext(conn))
+        static void Main(string[] args)
         {
-          foreach (var record in context.ExecuteQuery<TempRecord>("SELECT * FROM matsumoto_temp where snow >= 20 order by month"))
-          {
-            var sb = new StringBuilder();
-            sb.Append(record.month).Append("\t")
-              .Append(record.max).Append("\t")
-              .Append(record.max_q).Append("\t")
-              .Append(record.max_v).Append("\t")
-              .Append(record.min).Append("\t")
-              .Append(record.min_q).Append("\t")
-              .Append(record.min_v).Append("\t")
-              .Append(record.snow).Append("\t")
-              .Append(record.snow_e).Append("\t")
-              .Append(record.snow_q).Append("\t")
-              .Append(record.snow_v).Append("\t")
-              .AppendLine();
-            Console.WriteLine(sb.ToString());
-          }
+            var connString = ConfigurationManager.ConnectionStrings["db"]?.ConnectionString;
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                for (;;)
+                {
+                    var input = Console.ReadLine();
+                    input.Trim();
+                    if (input.ToLower().StartsWith("q"))
+                    {
+                        break;
+                    }
+                    if (input.Length == 0)
+                    {
+                        continue;
+                    }
+                    using (var cmd = new NpgsqlCommand(input, conn))
+                    {
+                        try
+                        {
+                            if (input.ToLower().StartsWith("select"))
+                            {
+                                using (var reader = cmd.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        var sep = "";
+                                        for (var i = 0; i < reader.FieldCount; i++)
+                                        {
+                                            Console.Write(sep + reader.GetValue(i).ToString());
+                                            sep = " ";
+                                        }
+                                        Console.WriteLine();
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                cmd.ExecuteNonQuery();
+                                Console.WriteLine("success and auto commit cmd[" + input + "]");
+                            }
+                        }
+                        catch (DbException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # PostgreSQLSample
 PostgreSQLをC#で使うサンプル
+
+## 接続プロバイダについて
+Npgsql or Odbc を使ってみた．
+使い勝手に判断つくほどやってない．
+NpgsqlはライセンスがPostgreSQL Licenceのため，PostgreSQLと同じでやりやすい面はあるかもしれない．
+ちなみにOdbcはLGPL.
+
+1. Npgsql
+    Nuget パッケージマネージャで入れるだけ．
+    PM> Install-Package Npgsql
+    参照 https://www.nuget.org/packages/Npgsql/
+
+1. Odbc
+    インストール後，Windows -> 管理メニュー -> データソース(ODBC)
+    ユーザーDSN -> 追加
+    データソースの新規作成 -> 名前 -> PostgreSQL
+    セットアップダイアログに各種情報を入力する．
+    データソース名が大事．プログラムで使用する．
